134fd93192a211bc5bdc195fab6ad3919d9892a1
rahul_katore <rahul.katore@neosofttech.com>
2018-11-29 20:14:50 +0530

    UI Part Completed

---
+++ 5KCoach/LapModel.swift
@@ -0,0 +1,9 @@
+//
+//  LapModel.swift
+//  5KCoach
+//
+//  Created by webwerks on 27/11/18.
+//  Copyright © 2018 webwerks. All rights reserved.
+//
+
+import Foundation
---
+++ 5KCoach/LapTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LapTableViewCell.swift
+//  5KCoach
+//
+//  Created by webwerks on 29/11/18.
+//  Copyright © 2018 webwerks. All rights reserved.
+//
+
+import UIKit
+
+class LapTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
